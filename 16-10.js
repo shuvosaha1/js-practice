19915a7082c1936b5a739e4cb359733b955e02b8
shuvosaha1 <102228562+shuvosaha1@users.noreply.github.com>
2022-08-02 17:58:06 +0600

    Add files via upload

---
+++ 16-10.js
@@ -0,0 +1,31 @@
+// Practice Problem 1
+
+var momGave = 1000;
+var cost = 700;
+var moneyBack = momGave - cost;
+console.log(moneyBack);
+
+// Practice Problem 2
+
+var math =  75.25;
+var bio = 65;
+var chem = 80;
+var phys = 35.45;
+var ban = 99.50;
+var tot = math + bio + chem + phys + ban;
+var avg = tot / 5;
+console.log(avg.toFixed(2));
+
+// Practice Problem 3
+
+var line1 = 'I am going to be';
+var line2 = 'an awesome web developer';
+var oneLine = line1 + ' ' + line2;
+console.log(oneLine);
+
+// Practice Problem 4
+
+var given = 119;
+var divBy = 5;
+var remainder = given % divBy;
+console.log(remainder);
