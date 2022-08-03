425246b5d2ec8d67446873b5ec8bcec92c69511d
shuvosaha1 <102228562+shuvosaha1@users.noreply.github.com>
2022-08-04 03:41:41 +0600

    Add files via upload

---
+++ 18_5/18_5-33-array-for-loop.js
@@ -0,0 +1,8 @@
+// ৩৩ তুমি এতদিন যা যা জিনিস শিখছো সেগুলার নাম দিয়ে 
+// একটা array বানাও। তারপর একটা for লুপ দিয়ে 
+// সেই array এর সব উপাদান কে আউটপুট হিসেবে দেখাও। 
+
+var learnt = ['html', 'css', 'js', 'etc'];
+for (var i = 0; i < learnt.length; i++) {
+    console.log(learnt[i]);
+}
---
+++ 18_5/18_5-34-array-while-loop.js
@@ -0,0 +1,12 @@
+// ৩৪. তুমি এতদিন পর্যন্ত যে যে মডেলের মোবাইল ফোন ইউজ করেছো 
+// সেগুলার নাম দিয়ে একটা array বানাও। তারপর একটা while লুপ দিয়ে 
+// সেই array এর উপাদান গুলা একটা একটা করে আউটপুট হিসেবে দেখাও 
+
+var mobile = ['khelna', 'motorola flip', 'china N72', 
+            'nokia 1100', 'nokia 3610 classic', 'lg cookie',
+            'redmi note 4x', 'redmi 4x'];
+var i = 0;
+while (i < mobile.length) {
+    console.log(mobile[i]);
+    i++;
+}
